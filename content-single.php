431d763210ea754f9b3b87ec99d24037bd44ab05
akrigline <andrew@akrigline.com>
2015-01-13 20:46:22 -0500

    Added loops for all image sequences

--- content-single.php
+++ content-single.php
@@ -5,7 +5,15 @@
 ?>
 
 <?php
-
+	$postID = get_the_ID();
+	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
+	$images = get_children(array(
+			'exclude' => get_post_thumbnail_id(),
+			'post_parent' => $post->ID,
+			'post_type' => 'attachment',
+			'post_mime_type' => 'image',
+			'numberposts' => -1 )
+	);
 ?>
 
 <div id="preload" class="sr-only">
@@ -18,47 +26,36 @@
   </div><!-- container -->
 
   <ul class="imageGallery list-unstyled">
-    <li>
-      <figure>
-        <img src="img/katiephoto1.jpg" />
-        <figcaption>
-        <h5>photo title</h5>
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <img src="img/katiephoto2.jpg" />
-        <figcaption>
-        <h5>photo title</h5>
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <img src="img/katiephoto3.jpg" />
-        <figcaption>
-        <h5>photo title</h5>
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <img src="img/katiephoto4.jpg" />
-        <figcaption>
-        <h5>photo title</h5>
-        </figcaption>
-      </figure>
-    </li>
+		<?php foreach ($images as $image) {
+					$attachmenturl = wp_get_attachment_url( $image->ID );
+					$attachmentimage = wp_get_attachment_image_src( $image->ID );
+					$attachmentalt = wp_get_attachment_image( $image->ID, 'alt' ); ?>		    
+		  <li>
+		    <figure>
+		      <img src="<?php echo $attachmentimage[0]; ?>" alt="<?php echo $attachmentalt; ?>">
+		      <figcaption>
+		        <h5><?php echo $attachmentalt; ?></h5>
+		      </figcaption>
+		    </figure>
+		  </li>
+		<?php } ?>
   </ul><!-- imageGallery-->
 
   <div class="container">
   <div class="thumbContainer">
     <ul class="thumbnails list-unstyled clearfix">
-      <li><a href="" data-slide-index="0"><img src="img/katiephoto1.jpg"></a></li>
-      <li><a href="" data-slide-index="1"><img src="img/katiephoto2.jpg"></a></li>
-      <li><a href="" data-slide-index="2"><img src="img/katiephoto3.jpg"></a></li>
-      <li><a href="" data-slide-index="3"><img src="img/katiephoto4.jpg"></a></li>
+	    <?php $count = -1; ?>
+      <?php foreach ($images as $image) {
+					$attachmenturl = wp_get_attachment_url( $image->ID );
+					$attachmentimage = wp_get_attachment_image_src( $image->ID, thumbnail );
+					$attachmentalt = wp_get_attachment_image( $image->ID, 'alt' ); ?>
+				<?php $count++; ?>
+			  <li>
+			    <a href="" data-slide-index="<?php echo $count; ?>">
+			      <img src="<?php echo $attachmentimage[0]; ?>" alt="<?php echo $attachmentalt; ?>">
+		      </a>
+			  </li>
+			<?php } ?>
     </ul><!-- thumbnails -->
   </div>
   </div><!-- container -->
